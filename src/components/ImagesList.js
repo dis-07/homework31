a83bca692d73220df6288c10f26883393afe0499
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-12-05 13:03:18 +0200

    initial commit

---
+++ src/components/ImagesList.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "helpers/axios";
+import PropTypes from "prop-types";
+
+const ImagesList = () => {
+  const [photos, setPhotos] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [currenPage, setCurrenPage] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`list?page=${currenPage}&limit=10`)
+      .then((data) => {
+        setPhotos((prevState) => [...prevState, ...data]);
+      })
+      .finally(setLoading(false));
+  }, [currenPage]);
+
+  const handleShowMore = () => {
+    setCurrenPage((value) => value + 1);
+  };
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <>
+      <p className="p">Image Gallery</p>
+      <div className="div">
+        {photos.map(({ id, download_url }) => (
+          // eslint-disable-next-line jsx-a11y/alt-text
+          <img className="img" key={id} src={download_url} />
+        ))}
+        <button className="btn" type="button" onClick={handleShowMore}>
+          Show more
+        </button>
+      </div>
+    </>
+  );
+};
+
+ImagesList.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+};
+
+export default ImagesList;
